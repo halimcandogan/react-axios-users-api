7aeda5d05b84c0795f43e6dbcb65dc9ef7119bb9
Halimcan Doğan <dogan.halimcan@hbv.edu.tr>
2022-05-22 14:20:00 +0300

    Update Cardcontainer.js

--- src/components/cardContainer/Cardcontainer.js
+++ src/components/cardContainer/Cardcontainer.js
@@ -6,10 +6,7 @@ export default function Cardcontainer() {
     <div>
         
          <Card />
-         <Card />
-         <Card />
-         <Card />
-         <Card />
+         
   
     
        
